98b726e772c6491fcc30b2bc282be81b0155ff39
Kevin Chang <kckaiwei@gmail.com>
2015-08-04 15:34:08 -0500

    Simple Stopwatch App for android

---
+++ app/src/main/java/com/example/kevin/stopwatch/StopwatchActivtiy.java
@@ -0,0 +1,104 @@
+package com.example.kevin.stopwatch;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+public class StopwatchActivtiy extends AppCompatActivity {
+
+    private int seconds = 0;
+    private boolean running;
+    private boolean wasRunning;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stopwatch_activtiy);
+        runTimer();
+        if (savedInstanceState != null){
+            seconds = savedInstanceState.getInt("seconds");
+            running = savedInstanceState.getBoolean("running");
+            wasRunning = savedInstanceState.getBoolean("wasRunning");
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_stopwatch_activtiy, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onSaveInstanceState(Bundle savedInstanceState){
+        savedInstanceState.putInt("seconds", seconds);
+        savedInstanceState.putBoolean("running", running);
+        savedInstanceState.putBoolean("wasRunning", wasRunning);
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        wasRunning = running;
+        running = false;
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        if (wasRunning){
+            running = true;
+        }
+    }
+
+    public void onClickStart(View view) {
+        running = true;
+    }
+
+    public void onClickStop(View view) {
+        running = false;
+    }
+
+    public void onClickReset(View view) {
+        seconds = 0;
+        running = false;
+    }
+
+    private void runTimer() {
+        final TextView timeView = (TextView) findViewById(R.id.time_view);
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+
+            public void run () {
+                int hours = seconds / 3600;
+                int minutes = (seconds % 3600) / 60;
+                int secs = seconds % 60;
+
+                String time = String.format("%d:%02d:%02d", hours, minutes, secs);
+                timeView.setText(time);
+                if (running) {
+                    seconds++;
+                }
+                handler.postDelayed(this,1000);
+            }
+        });
+    }
+}
